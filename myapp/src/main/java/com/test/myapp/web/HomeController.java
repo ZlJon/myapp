6234b149f6d21daabfa717b3dd1a088312fb693a
minseop <kim5ys@naver.com>
2023-04-14 11:38:07 +0900

    버전1

---
+++ myapp/src/main/java/com/test/myapp/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.test.myapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String home () {
+    return "home";
+  }
+}
